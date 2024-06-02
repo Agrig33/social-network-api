eb86d4b4c224b00a926c3882fc61b24dec5b3c7c
{airin grigorian} <agrigorian33@gmail.com>
2024-06-02 00:29:49 -0700

    created userRoutes in routes folder/api

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const {
-    getUsers,
-    getSingleUser,
-    createNewUser,
-    updateUser,
-    deleteUser,
-    addNewFriend,
-    deleteFriend,
-    deleteAllFriends
-} = require('../../')
+const { getAllUsers,getSingleUser,createNewUser,deleteUser,updateUser,addFriend,deleteFriend } = require('../../controllers/userControllers');
+
+router.route('/').get(getAllUsers).post(createNewUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friend/friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
