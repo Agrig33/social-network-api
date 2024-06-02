edd59e55c1d5316bb893a78aef45252ff9bd6175
{airin grigorian} <agrigorian33@gmail.com>
2024-06-02 00:48:34 -0700

    revised typo on line 5 and 6 in routes/api/index.js users, to user and thoughts to thought

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const { getAllThoughts, getSingleThought, createThought, deleteThought, updateThought, 
+    addAReaction, deleteReaction } = require('../../controllers/thoughtControllers');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reaction').post(addAReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
