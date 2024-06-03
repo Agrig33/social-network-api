b407c167c22cfb21c510abfcb5a3cc4199f81195
{airin grigorian} <agrigorian33@gmail.com>
2024-06-02 23:36:01 -0700

    revised line 12 on server.js, it had incorrect path

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+connectDB();
+
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', require('./routes'));
 
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`API server is running on port ${PORT}!`);
+        console.log(`API server is running on http://localhost:${PORT}`);
     });
 });
