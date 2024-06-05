4cd3f3182941c3e1ce1ac4d0c6c7d2d32de3631e
{airin grigorian} <agrigorian33@gmail.com>
2024-06-05 01:41:13 -0700

    revised connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const { connect, connection } = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/socialnetworkDB');
-// dotenv.config();
+dotenv.config();
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkDB');
-//             console.log('Success! You are now connected to MongoDB.');
-//     } catch (err) {
-//         console.error('Error! Cannot connect to MongoDB', err);
-//     }
-// };
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkDB');
+            console.log('Success! You are now connected to MongoDB.');
+    } catch (err) {
+        console.error('Error! Cannot connect to MongoDB', err);
+    }
+};
 
-// module.exports = connectDB;
+module.exports = connectDB;
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
