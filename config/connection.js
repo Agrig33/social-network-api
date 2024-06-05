0b7953ffb0d6a28cbc413fbccd94ebc1da6684f0
{airin grigorian} <agrigorian33@gmail.com>
2024-06-05 00:16:48 -0700

    revised thoughtcontrollers file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+const { connect, connection } = require('mongoose');
 
-dotenv.config();
+mongoose.connect('mongodb://localhost:27017/socialnetworkDB');
+// dotenv.config();
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkDB');
-            console.log('Success! You are now connected to MongoDB.');
-    } catch (err) {
-        console.error('Error! Cannot connect to MongoDB', err);
-    }
-};
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkDB');
+//             console.log('Success! You are now connected to MongoDB.');
+//     } catch (err) {
+//         console.error('Error! Cannot connect to MongoDB', err);
+//     }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
 
+module.exports = mongoose.connection;
--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,128 +1,127 @@
-const { Thought } = require('../models/Thought');
+const { Thought, User } = require('../models');
 
-
-
-// module.exports = { 
-    const getAllThoughts = async (req, res) => {
+    const thoughtCon = {
+        async getAllThoughts(req, res) {
         try {
-            const thoughts = await Thought.find();
+            const thoughtData = await Thought.find();
             // return res.status(200).json(thoughts);
-            return res.json(thoughts);
-
-        } catch (err) {
+            return res.json(thoughtData);
+    } catch (err) {
             console.log(err);
-            return res.status(500).json({ message: 'Oops, there was an error retrieving thoughts.', error: err.message });
+            return res.status(500).json({ message: 'Oops, there was an error retrieving thoughts.', err });
         }
-    };
+    },
     // module.exports = { 
     //     getAllThoughts,
 
-const getSingleThought = async (req, res) => {
+async getSingleThought(req, res) {
     try {
-        const thought = await Thought.findOne({ _id: req.params.thoughtId });
-       if (!thought) {
+        const thoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+       if (!thoughtData) {
             // return res.status(200).json(thought); //this is new
             return res.status(404).json({ message: 'Error, no thought found with that ID.'});
         }
-        return res.status(200).json(thought);
+        return res.status(200).json(thoughtData);
     } catch (err) {
         console.log(err);
-        return res.status(500).json({ message: 'Oops, there was an error retrieving a thought.', error: err.message });
+        return res.status(500).json({ message: 'Oops, there was an error retrieving a thought.', err });
     }
-};
+},
 // module.exports = { 
 //     getSingleThought,
 
-const createThought = async (req, res) => {
+async createThought(req, res) {
     try {
-        const thought = await Thought.create(req.body);
-        const user = await User.findOneAndUpdate(
+        const thoughtData = await Thought.create(req.body);
+        const userData = await User.findOneAndUpdate(
             { _id: req.body.userId },
-            { $addToSet: { thoughts: thought._id }},
+            { $push: { thoughts: thoughtData._id }},
             { new: true });
 
-        if (!user) {
-            return res.status(404).json({ message: 'Error, no user found with that ID.'});
-        }
-        return res.json(user);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json(err);
+        if (!userData) {
+      return res.status(404).json({ message: 'Error, user was not found.'});
     }
-};
-
-const deleteThought = async (req, res) => {
-    try {
-        const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
-
-    if (!thought) {
-            return res.status(404).json({ message: 'Error, no thought found with that ID.'});
-        }
-        return res.status(200).json({ message: 'Success! Thought has been deleted.'});
-    } catch (err) {
+        res.json({ message: 'Success! Thought was created successfully!'}); 
+     } catch (err) {
         console.log(err);
-        return res.status(500).json(err);
+        return res.status(500).json({ message: 'Oops, there was an error creating the thought.', err });
     }
-};
+},
 
-const updateThought= async (req, res) => {
+async updateThought(req, res) {
     try {
-        const thought = await Thought.findOneAndUpdate(
+        const thoughtData = await Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
             { $set: req.body },
             { runValidators: true, new: true });
             
-    if (!thought) {
+    if (!thoughtData) {
         return res.status(404).json({ message: 'Error, no thought found with that ID.'});
     } 
-        return res.status(200).json(thought);
+        return res.json(thoughtData);
     } catch (err) {
         console.log(err);
-        return res.status(500).json(err);
-}
+        return res.status(500).json({ message: 'Oops, there was an error updating the thought.', err });
+    }
+},
 
-};
-
-const addAReaction = async (req, res) => {
+async deleteThought(req, res) {
     try {
-        const reaction = await Thought.findOneAndUpdate(
+        const thoughtData = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+    if (!thoughtData) {
+            return res.status(404).json({ message: 'Error, no thought found with that ID.'});
+        }
+        
+        const userData = await User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId }},
+            { new: true }
+        );
+        
+    if(!userData) {
+        return res.status(404).json({ message: 'Error, user was not found.' });
+    }
+        return res.json({ message: 'Success! Thought has been deleted.'});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: 'Oops, there was an error deleting the thought.', err });
+    }
+},
+
+async addAReaction(req, res) {
+    try {
+        const thoughtData = await Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
             { $addToSet: {reactions: req.body }},
             { runValidators: true, new: true });
 
-    if (!reaction) {
+    if (!thoughtData) {
         return res.status(404).json({ message: 'Error, there was no thought found with that ID.'});
         }
-        res.json(reaction);
+        res.json(thoughtData);
     } catch (err) {
         console.log(err);
-        return res.status(500).json(err);
+        return res.status(500).json({ message: 'Oops, there was an error adding the reaction.', err });
     }
-};
+},
 
-const deleteReaction = async (req, res) => {
+async deleteReaction(req, res) {
     try {
-        const reaction = await Thought.findOneAndUpdate(
+        const thoughtData = await Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
-            { $pull: { reactions: {_id: req.params.reactionId} }},
+            { $pull: { reactions: {reactionId: req.params.reactionId} }},
             { runValidators: true, new: true });
 
-    if (!reaction) {
+    if (!thoughtData) {
         return res.status(404).json({ message: 'Error, there was no thought found with that ID.'});
     }
+        return res.json(thoughtData);
     } catch (err) {
         console.log(err);
-        return res.status(500).json(err);
+        return res.status(500).json({ message: 'Oops, there was an error deleting the reaction.', err });
     }
-    // },
+    },
 };
 
-module.exports = {
-    getAllThoughts,
-    getSingleThought,
-    createThought,
-    deleteThought,
-    updateThought,
-    addAReaction,
-    deleteReaction
-};
+module.exports = thoughtCon;
